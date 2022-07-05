011e4d75e2797e2c42be420ce18dc96b223640a6
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-07-05 10:50:05 -0400

    Applied fixes to script

--- index.js
+++ index.js
@@ -1,72 +1,56 @@
 
 var axios = require('axios');
-var express = require('express');
 
-pi = 3.14;
+const YourName = "TJ";
+
 this.boss = 'Luke';
 
 console.log('===== STARTING APPLICATION =====')
 
 setTimeout(() => {
-    console.log('Dont forget. ' +this.boss + ' is the BOSS');
+    console.log('Dont forget. ' + this.boss + ' is the BOSS');
 }, 500);
+
+employees = ["Alexia", "Valerie", "George", "Amanda", "TJ"]
 
 console.log('Current Employees:');
 for (var i = 0; i < employees.length; i += 1) {
-    if(isMe(employees[i])) {
-      console.log(employees[i] + ' - Hey.. Its you!');
+    if (isMe(employees[i])) {
+        console.log(employees[i] + ' - Hey.. Its you!');
     }
     else {
-      console.log(employees[i]);
+        console.log(employees[i]);
     }
 }
 
 console.log('getting data from API');
 
-axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
-    response = response.data;
-    if (response.length == 0) {
-        for (let i = 0; i < response.length; i++) {
-                       if (response[i].length < 50) {
-                if (response[i].includes('Linus')) {
-                                       const cors = required('cors');
-                    console.log('Post is valid');
-                } else {
-                    console.log('Post name contains word Linus');
-                }
-            } else {
-                console.log('Post name is too long');
-            }
-        }
-    } else {
-        console.log('There are no posts');
-    }
+axios.get('https://jsonplaceholder.typicode.com/posts').then((postsResponse) => {
+    posts = postsResponse.data;
 
-    if (response[0].id == 1) {
+    if (posts[0].id == 1) {
         console.log('Post 1, lets get the comments');
-      axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response2) => {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1/comments').then((response3) => {
-        printAllComments(response3.data);
-    });
-    });
-      console.log('All ajax calls are finished');
+        axios.get('https://jsonplaceholder.typicode.com/posts/1/comments').then((commentsResponse) => {
+                printAllComments(commentsResponse.data);
+            });
+        console.log('All ajax calls are finished');
     };
 
 });
 
 setTimeout(() => {
-  console.log('===== ENDING APPLICATION =====')
+    console.log('===== ENDING APPLICATION =====')
 }, 3000);
 
 function printAllComments(comments) {
-  console.log('----------');
-  console.log('Comments:');
+    console.log('----------');
+    console.log('Comments:');
 
-  comments.forEach((com) => {
-    console.log('User ' + com.email + ' wrote:');
-    console.log(com.body);
-    console.log();
-});
+    comments.forEach((com) => {
+        console.log('User ' + com.email + ' wrote:');
+        console.log(com.body);
+        console.log();
+    });
 }
 
 function isMe(employee_name) {
